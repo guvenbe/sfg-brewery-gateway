521c936c66ba2986e2b64b3a1e50eac0c6595c2d
John Thompson <john@springframework.guru>
2020-03-03 20:44:30 -0500

    inital project add

---
+++ src/main/java/com/github/sfgbeerworks/sfgbrewerygateway/SfgBreweryGatewayApplication.java
@@ -0,0 +1,35 @@
+package com.github.sfgbeerworks.sfgbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SfgBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgBreweryGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+
+		return builder.routes()
+				.route(r -> r.path("/api/v1/beer/*", "beerUpc/*")
+						.filters(f -> f.rewritePath("/(?<remains>/?.*)", "/${remains}"))
+						.uri("http://localhost:8080")
+						.id("beer-service"))
+				.route(r -> r.path("/api/v1/beer/*/inventory" )
+						.filters(f -> f.rewritePath("/(?<remains>/?.*)", "/${remains}"))
+						.uri("http://localhost:8082")
+						.id("inventory-service"))
+				.route(r -> r.path(("/api/v1/customers/**"))
+						.filters(f -> f.rewritePath("/(?<remains>/?.*)", "/${remains}"))
+						.uri("http://localhost:8081")
+						.id("order-service"))
+				.build();
+
+	}
+}
