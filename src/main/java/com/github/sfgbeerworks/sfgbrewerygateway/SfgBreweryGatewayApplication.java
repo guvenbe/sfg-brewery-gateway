c01b4b1a65a2baa028b481613e2259312e4c1059
John Thompson <john@springframework.guru>
2020-05-24 13:37:42 -0400

    adding gateway service

--- src/main/java/com/github/sfgbeerworks/sfgbrewerygateway/SfgBreweryGatewayApplication.java
+++ src/main/java/com/github/sfgbeerworks/sfgbrewerygateway/SfgBreweryGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SfgBreweryGatewayApplication {
@@ -14,10 +15,22 @@ public class SfgBreweryGatewayApplication {
 	}
 
 	@Bean
+	public RouteLocator googleRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/google")
+						.filters(f -> f.rewritePath("/google(?<segment>/?.*)", "/${segment}"))
+					.uri("https://google.com")
+						.id("Google")
+				)
+				.build();
+	}
+
+	@Profile("!local-discovery")
+	@Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
 
 		return builder.routes()
-				.route(r -> r.path("/api/v1/beer/*", "beerUpc/*")
+				.route(r -> r.path("/api/v1/beer/*", "/api/v1/beerUpc/*")
 						.uri("http://localhost:8080")
 						.id("beer-service"))
 				.route(r -> r.path("/api/v1/beer/*/inventory" )
@@ -27,6 +40,48 @@ public class SfgBreweryGatewayApplication {
 						.uri("http://localhost:8081")
 						.id("order-service"))
 				.build();
-
 	}
+
+	@Profile("local-discovery")
+	@Bean
+	public RouteLocator gatewayRoutesLocalDiscovery(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/api/v1/beer/*", "/api/v1/beerUpc/*")
+						.uri("lb://beer-service")
+						.id("beer-service"))
+				.route(r -> r.path("/api/v1/beer/*/inventory")
+						.filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+								.setFallbackUri("forward:/inventory-failover")
+								.setRouteId("inventory-failover-route")))
+						.uri("lb://inventory-service")
+						.id("inventory-service"))
+				.route(r -> r.path("/inventory-failover/**")
+					  //  .filters(f -> f.rewritePath("/inventory-failover(?<segment>/?.*)", "/${segment}"))
+						.uri("lb://inventory-failover")
+						.id("inventory-failover-service"))
+				.route(r -> r.path("/api/v1/customers/**")
+						.uri("lb://order-service")
+						.id("order-service"))
+				.build();
+	}
+
+
+	//@Configuration
+	//public class Resilience4JConfiguration {
+
+		//@Profile("local-discovery")
+//		@Bean
+//		public FallbackHeadersGatewayFilterFactory fallbackHeadersGatewayFilterFactory() {
+//			return new FallbackHeadersGatewayFilterFactory();
+//		}
+
+		//@Profile("local-discovery")
+//		@Bean
+//		public SpringCloudCircuitBreakerFilterFactory springCloudCircuitBreakerFilterFactory(
+//				ReactiveResilience4JCircuitBreakerFactory reactiveCircuitBreakerFactory,
+//				ObjectProvider<DispatcherHandler> dispatcherHandlers) {
+//			return new SpringCloudCircuitBreakerResilience4JFilterFactory(reactiveCircuitBreakerFactory, dispatcherHandlers);
+//		}
+
+	//}
 }
